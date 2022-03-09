055ea683664e087c5eeec01d49002cb978bdd35e
fearm3 <ibraaa1628@gmail.com>
2022-03-10 00:50:54 +0300

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Head from "next/head";
+
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
 
@@ -7,6 +9,13 @@ import Hero from "../components/home-page/hero";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Fearm3' Blog</title>
+        <meta
+          name="description"
+          content="I post programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
